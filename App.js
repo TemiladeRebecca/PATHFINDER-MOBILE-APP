998c794a462019010f31e9dc7e3462f1154d87c7
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-11-12 20:14:34 +0100

    Configure primary navigation for app screens

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
-import * as React from 'react';
+import 'react-native-gesture-handler';
+import { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import PagerComponent from './components/PagerComponent';
+import AnimatedSplashScreen from './screens/AnimatedSplashScreen';
+import GettingStartedScreen from './screens/GettingStarted';
+import LoginPage from './components/LoginPage';
 
-import HomeScreen from './screens/HomeScreen';
-import DetailScreenOne from './screens/DetailScreenOne';
-import DetailScreenTwo from './screens/DetailScreenTwo';
-import DetailScreenThree from './screens/DetailScreenThree';
-
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="DetailOne" component={DetailScreenOne} />
-        <Stack.Screen name="DetailTwo" component={DetailScreenTwo} />
-        <Stack.Screen name="DetailThree" component={DetailScreenThree} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    // State to track if splash animation is complete
+    const [isSplashAnimationComplete, setSplashAnimationComplete] = useState(false);
+
+    // Conditional rendering: Show AnimatedSplashScreen first
+    if (!isSplashAnimationComplete) {
+        return (
+            <AnimatedSplashScreen onFinish={() => setSplashAnimationComplete(true)} />
+        );
+    }
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Pager">
+                <Stack.Screen
+                    name="Pager"
+                    component={PagerComponent}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="GettingStarted"
+                    component={GettingStartedScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="LoginPage"
+                    component={LoginPage}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
