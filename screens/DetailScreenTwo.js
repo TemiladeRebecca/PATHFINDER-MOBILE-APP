02a3a18182d2068466ab82b2251f37f38eb21db4
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-11-12 20:06:46 +0100

    Add detail screens for displaying specific content and navigation.Set up navigation between main and detail screens

--- screens/DetailScreenTwo.js
+++ screens/DetailScreenTwo.js
@@ -1,29 +1,21 @@
-import { View, Image, StyleSheet, Text, Pressable } from 'react-native';
-import PrimaryButton from '../components/PrimaryButton';
+import { View, Image, StyleSheet, Text } from 'react-native';
 
-export default function DetailScreenTwo({navigation}) {
+export default function DetailScreenTwo() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>PATHFINDER</Text>
-            <Image 
-                source={require('../assets/images/image2.jpg')}
-                style={styles.image} 
-                resizeMode="contain" 
-            />
+            <View style={styles.imageWrapper}>
+                <Image 
+                    source={require('../assets/images/image2.jpg')}
+                    style={styles.image} 
+                    resizeMode="contain" 
+                />
+            </View>
             <View style={styles.textWrapper}>
                 <Text style={styles.descriptionText}>Report Health Concerns Instantly</Text>
                 <Text style={styles.subText}>
                     Contribute to community health by reporting symptoms and concerns.
                     Your reports help identify potential outbreaks early.
                 </Text>
-            </View>
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onPress={() => navigation.navigate('DetailThree')}>Next</PrimaryButton>
-                </View>
-                <Pressable style={styles.skipButton}>
-                    <Text style={styles.skipButtonText}>Skip</Text>
-                </Pressable>    
             </View>
         </View>
     );
@@ -32,21 +24,22 @@ export default function DetailScreenTwo({navigation}) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
     },
-    title: {
-        color: '#5EA2DC',
-        fontSize: 32,
-        fontWeight: 'bold',
-        textAlign: 'center',
+    imageWrapper: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 30
     },
     image: {
-        width: '100%',
-        height: '50%',
-        marginBottom: 5, 
+        width: 500, 
+        height: 320,
     },
     textWrapper: {
-        marginBottom: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     descriptionText: {
         fontSize: 18,
@@ -57,29 +50,6 @@ const styles = StyleSheet.create({
     subText: {
         textAlign: 'center',
         color: '#303640',
-        lineHeight: 20
+        lineHeight: 20,
     },
-    buttonsContainer: {
-        flexDirection: 'row',
-        marginTop: '30%',
-        marginLeft: 10,
-        width: '90%',
-    },
-    
-    buttonContainer: {
-        width: '65%',
-        marginRight: 25
-    },
-
-    skipButton: {
-        width: '20%',
-        paddingVertical: 15,
-    },
-
-    skipButtonText: {
-        color: '#5EA2DC',
-        fontSize: 16,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    }
 });
