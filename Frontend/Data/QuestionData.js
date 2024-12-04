7a1a24716ee1130b0d449777c1b455c236f27f15
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-12-04 15:09:52 +0100

    create file with predefined questions for outbreaks

---
+++ Frontend/Data/QuestionData.js
@@ -0,0 +1,45 @@
+const questionData = {
+    Diarrhea: [
+      {
+        id: 1,
+        question: "What is the main source of water in this area?",
+        placeholder: "e.g., Borehole, River, Contaminated",
+        type: "text",
+      },
+      {
+        id: 2,
+        question: "How many cases of diarrhea have been reported recently?",
+        placeholder: "Enter a number",
+        type: "number",
+      },
+      {
+        id: 3,
+        question: "Are there any sanitation issues in this area? (Yes/No)",
+        placeholder: "Yes or No",
+        type: "text",
+      },
+    ],
+    Cholera: [
+      {
+        id: 1,
+        question: "What is the main source of water in this area?",
+        placeholder: "e.g., Borehole, River, Contaminated",
+        type: "text",
+      },
+      {
+        id: 2,
+        question: "How many cases of cholera have been reported recently?",
+        placeholder: "Enter a number",
+        type: "number",
+      },
+      {
+        id: 3,
+        question: "Is the drinking water treated before use?",
+        placeholder: "e.g., boiled, chlorinated, filtered",
+        type: "text",
+      },
+    ],
+  };
+  
+  export default questionData;
+  
