edc19aa0314cc847e25ae99562635ab4ff35b4c5
TemiladeRebecca <temilade.rebecca20@gmail.com>
2024-12-08 13:41:02 +0100

    feat: Add floating button and toggleMapUpdateHandler to control Map screen visibility in App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import PagerComponent from './components/PagerComponent';
@@ -7,13 +7,27 @@ import GettingStartedScreen from './screens/GettingStarted';
 import LoginPage from './components/LoginPage';
 import QuestionsScreen from './screens/QuestionsScreen';
 import DiseasesScreen from './screens/DiseasesScreen';
+import MapScreen from './screens/MapScreen';
+import FloatingButton from './components/FloatingButton';
 
 const Stack = createStackNavigator();
 
 export default function App() {
 
+    const [isMapUpdated, setIsMapUpdated] = useState(false);
+    const navigationRef = useRef();
+
+    const ToggleMapUpdateHandler = () => {
+        if (isMapUpdated) {
+            navigationRef.current?.goBack()
+        } else {
+            navigationRef.current?.navigate('MapScreen')
+        }
+        setIsMapUpdated((prevValue) => !prevValue)
+    }
+
     return (
-        <NavigationContainer>
+        <NavigationContainer ref={navigationRef}>
             <Stack.Navigator initialRouteName="Pager">
                 <Stack.Screen
                     name="Pager"
@@ -40,7 +54,13 @@ export default function App() {
                 component={QuestionsScreen}
                 options={{ headerShown: false }}
             />
+            <Stack.Screen 
+                name="MapScreen"
+                component={MapScreen}
+                options={{ headerShown: false }}
+            />
             </Stack.Navigator>
+            <FloatingButton onPress={ToggleMapUpdateHandler} label={isMapUpdated ? 'Hide Updates' : 'Show Map Updates'} />
         </NavigationContainer>
     );
 }
